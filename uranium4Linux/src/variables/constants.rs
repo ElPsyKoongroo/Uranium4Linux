1cb9a7216252ac81549d38915dc76fd1e47997cf
sergious234 <sergious234@gmail.com>
2022-04-04 02:10:30 +0200

    MENU const

--- uranium4Linux/src/variables/constants.rs
+++ uranium4Linux/src/variables/constants.rs
@@ -1,3 +1,12 @@
 #![allow(dead_code)]
 pub const RINTH: &str = "RINTH";
 pub const CURSE: &str = "CURSE";
+
+
+pub const MENU: &str = 
+"
+mod  + <number>\n
+page + <number>\n
+path \n                
+make \n        
+";
