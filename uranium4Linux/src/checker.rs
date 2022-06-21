e7fa293fcf120453dbe9d8803351969671376cfd
sergious234 <sergious234@gmail.com>
2022-06-21 17:03:32 +0200

    Instead of stop, panic

--- uranium4Linux/src/checker.rs
+++ uranium4Linux/src/checker.rs
@@ -4,28 +4,29 @@ use std::fmt::Debug;
 /// Given a Result<T, E> it checks if Ok() or Err(), 
 /// if Ok it returns T
 /// if Err it calls manage_error
-pub fn check<T, E>(value: Result<T, E>, stop: bool, show_error: bool, msg: &str) -> Option<T>
+pub fn check<T, E>(value: Result<T, E>, panic: bool, show_error: bool, msg: &str) -> Option<T>
 where
     E: Debug,
 {
     match value {
         Ok(e) => Some(e),
         Err(error) => {
-            manage_error(error, stop, show_error, msg);
-            None
+            manage_error(error, show_error, msg);
+            if !panic {
+                None
+            } else {
+                panic!();
+            }
         }
     }
 }
 
-fn manage_error<E>(error: E, stop: bool, show_error: bool, msg: &str)
+fn manage_error<E>(error: E, show_error: bool, msg: &str)
 where
     E: Debug,
 {
     eprintln!("{msg}");
     if show_error {
         eprintln!("Next error ocurred in runtime: {:?}\n\n", error);
-    }
-    if stop {
-        let _ = easy_input::input("Press enter to continue...", String::from(""));
     }
 }
