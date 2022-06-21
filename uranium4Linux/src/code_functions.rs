4459d1a04536c39e41c53a0872aab79b9c60ce8a
sergious234 <sergious234@gmail.com>
2022-06-21 17:04:05 +0200

    usize as error

--- uranium4Linux/src/code_functions.rs
+++ uranium4Linux/src/code_functions.rs
@@ -1,17 +1,20 @@
 use std::{path::Path, error::Error};
 use crate::modpack_downloader::{loader::ModPackDownloader, updater::update_modpack};
 
-pub async fn download_modpack(modpack: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn download_modpack<'a>(modpack: &str, path: &'a str) -> Result<(), usize> {
+    // if !Path::new(path).exists() {
+    //     return Err(Box::<dyn Error>::from(format!(
+    //         "{} is not a valid path !",
+    //         path
+    //     )));
+    // };
     if !Path::new(path).exists() {
-        return Err(Box::<dyn Error>::from(format!(
-            "{} is not a valid path !",
-            path
-        )));
+        return Err(1)
     };
     let mut modpack_loader = ModPackDownloader::new();
     modpack_loader.set_path(String::from(path));
     modpack_loader.load_pack(modpack);
-    modpack_loader.start().await?;
+    modpack_loader.start().await.unwrap();
     println!("\n\n");
     Ok(())
 }
