eb99f014d72ce4e9a85cef5e77a855547695d8ca
sergious234 <sergious234@gmail.com>
2022-06-21 17:08:47 +0200

    change return type

--- uranium4Linux/src/main.rs
+++ uranium4Linux/src/main.rs
@@ -13,15 +13,23 @@ use std::env;
 use variables::constants::*;
 use zipper::pack_unzipper::unzip_pack;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
 
+/*
+    TODO: 
+        - If a mod cant be found on Rinth, add the .jar to the modpack.
+        - Unzip de modpack in minecraft folder.
+        - Add '.' to the modpack name. (now it writes Name + "zip" instead of Name + ".zip")
+
+*/
+
+#[tokio::main]
+async fn main() -> Result<(), usize> {
+    let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
         "-d" => return download_modpack(&args[2], &args[3]).await,
         "-u" => update(args[2].as_str()).await,
         "-m" => make_modpack(args[2].as_str()).await,
-        "-t" => unzip_pack(&args[2]).unwrap(),
+        "-t" => unzip_pack(&args[2], &args[3]).await.unwrap(),
         "-h" => println!("{}", HELP),
         _    => println!("{}", "Invalid arguments")
     }
