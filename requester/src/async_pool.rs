100ba1b191d85343bb5ddf87dc7e2fd85fb0dd45
sergious234 <sergious234@gmail.com>
2022-06-27 20:24:50 +0200

    Better async pool

--- requester/src/async_pool.rs
+++ requester/src/async_pool.rs
@@ -1,40 +1,47 @@
-use std::time::Duration;
+use std::{time::Duration, collections::HashMap};
+use tokio::{task::JoinHandle, time};
 
-use reqwest::Response;
-use tokio::{task::{JoinHandle}, time};
+const PRE_TIME: u64 = 100;
 
-pub struct AsyncPool {
-    request_pool: Vec<JoinHandle<Response>>,
-    done_request: Vec<Response>,
+pub struct AsyncPool<T> {
+    request_pool: Vec<JoinHandle<T>>,
+    items: usize,
     not_done_request: Vec<usize>,
+    ordered_requests: HashMap<usize, T>
 }
 
 
-impl AsyncPool {
+impl<T> AsyncPool<T> {
 
-    pub fn new() -> AsyncPool {
+    pub fn new() -> AsyncPool<T> {
         AsyncPool {
             request_pool: Vec::new(),
-            done_request: Vec::new(),
+            items: 0,
             not_done_request: Vec::new(),
+            ordered_requests: HashMap::new()
         }
     }
 
-    pub fn push_request(&mut self, request: JoinHandle<Response>) {
+    pub fn push_request(&mut self, request: JoinHandle<T>) {
         self.request_pool.push(request);
     }
 
+    pub fn push_request_vec(&mut self, mut _requests: Vec<JoinHandle<T>>){
+        self.request_pool.append(&mut _requests);
+    }
+
     pub async fn start(&mut self){
-        self.done_request = Vec::with_capacity(self.request_pool.len());
-        self.not_done_request = Vec::from_iter(0..self.request_pool.len());
+        self.items = self.request_pool.len();
+        self.not_done_request = Vec::from_iter(0..self.items);
+        time::sleep(Duration::from_millis(PRE_TIME)).await;
         while !self.not_done_request.is_empty() {
             self.request_loop().await;
         }
     }
 
     async fn request_loop(&mut self) {
-        for i in self.not_done_request.clone().iter() {
-            let sleep = time::sleep(Duration::from_millis(50));
+        for i in self.not_done_request.clone().into_iter() {
+            let sleep = time::sleep(Duration::from_millis(20));
             tokio::pin!(sleep);
 
             tokio::select! {
@@ -42,17 +49,21 @@ impl AsyncPool {
                     continue;
                 }
                 
-                res = &mut self.request_pool[*i] => {
-                    self.done_request.push(res.unwrap());
-                    self.not_done_request.retain(|&x| x != *i);
-
+                res = &mut self.request_pool[i] => {
+                    self.ordered_requests.insert(i, res.unwrap());
+                    self.not_done_request.retain(|&x| x != i);
                 }
             }
         }
     }
 
-    pub fn get_done_request(self) -> Vec<Response> {
-        self.done_request
+    pub fn get_done_request(&mut self) -> Vec<T> {
+        let mut done_requests = Vec::new();
+        for i in 0..self.items{
+            let value = self.ordered_requests.remove(&i).unwrap();
+            done_requests.push(value);
+        }
+        done_requests
     }
 }
 
